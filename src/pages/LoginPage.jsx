814f60b53631d5c413096651695fee09a8aa0df2
Raihanah1312 <hanaraihanah13@gmail.com>
2024-08-01 11:47:13 +0700

    login page, register page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -31,24 +31,24 @@ export default function LoginPage() {
 
         <div className="h-full  w-[60%] m-6 space-y-8 flex flex-col">
           <div>
-            <h1 className="text-emptracky-blue font-bold text-[32px]">WELCOME BACK!</h1>
-            <h1 className="text-emptracky-darkgray text-[24px] mb-2">Please login to your account</h1>  
+            <h1 className="text-emptracky-blue font-bold text-4xl">WELCOME BACK!</h1>
+            <h1 className="text-emptracky-darkgray text-2xl mb-2">Please login to your account</h1>  
           </div>
 
           <div>
-            <h1 className="text-emptracky-darkgray text-[24px]">Username</h1>
+            <h1 className="text-emptracky-darkgray text-2xl">Username</h1>
             <input className="shadow-md border border-emptracky-blue w-full h-12 bg-emptracky-f5 rounded-[30px] px-5 outline-emptracky-blue" />
-            <h1 className="text-emptracky-darkgray text-[24px] mt-4">Password</h1>
+            <h1 className="text-emptracky-darkgray text-2xl mt-4">Password</h1>
             <input className="shadow-md border border-emptracky-blue w-full h-12 bg-emptracky-f5 rounded-[30px] px-5 outline-emptracky-blue" />
             
           </div>
 
           <div className="w-full flex flex-col gap-3 justify-center items-center mt-12">
             <button className="shadow-md w-full h-12 bg-emptracky-blue rounded-[50px]">
-              <h1 className="text-emptracky-f1 text-[26px]" onClick={() => navigate("/home")}>Sign in</h1>
+              <h1 className="text-emptracky-f1 text-2xl" onClick={() => navigate("/home")}>Sign in</h1>
             </button>
 
-            <h1 className="text-emptracky-darkgray text-[20px] ">Already have an account?
+            <h1 className="text-emptracky-darkgray text-lg text-center ">Already have an account?
               <span className= "text-emptracky-blue text-decoration-line: underline hover:cursor-pointer w-fit transition-all duration-200 ml-2" onClick={() => navigate("/register")}>
               Register here!
               </span>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -8,10 +8,10 @@ const RegisterPage = () => {
 const navigate = useNavigate();
 
   return (
-    <div overflow-hidden font-poppins gap-12>
-    <div className="h-screen w-screen flex justify-center items-center bg-no-repeat bg-cover bg-register ">
+    <div>
+    <div className="min-h-screen w-screen flex justify-center items-center bg-no-repeat bg-cover bg-register  overflow-hidden font-poppins gap-12 ">
   
-      <div className='w-[500px] justify-center items-center text-center text-emptracky-fd hidden rounded-l-[20px] md:block object-cover'>
+      <div className='w-[500px] justify-center items-center text-center m-6 space-y-8 text-emptracky-fd hidden rounded-[20px] md:block object-cover'>
         <div className='flex gap-3'>
           <img src={logo} />
           <h1 className=" font-bold text-[64px]">EmpTracky</h1>
@@ -19,23 +19,23 @@ const navigate = useNavigate();
         <p className='text-3xl'>Save time and manage attendance more efficiently</p>
       </div>
 
-      <div className="h-fit w-[583px] bg-emptracky-fd rounded-[20px] flex">
+      <div className="relative h-full w-[583px] bg-emptracky-fd rounded-[20px] m-6 space-y-8 flex flex-col">
 
         <div className="h-full w-full justify-center items-center p-12">
           <div className='flex w-full justify-center items-center'>
-            <h1 className="text-emptracky-blue font-bold text-[32px] mb-2">Register</h1>
+            <h1 className="text-emptracky-blue font-bold text-4xl mb-2">Register</h1>
           </div>
 
           <div>
-            <h1 className="text-emptracky-darkgray text-[24px]">Userame</h1>
+            <h1 className="text-emptracky-darkgray text-2xl">Userame</h1>
             <input className="shadow-md w-full h-12 bg-emptracky-f5 rounded-[30px] px-5 border border-emptracky-blue" />
-            <h1 className="text-emptracky-darkgray text-[24px]">Email</h1>
+            <h1 className="text-emptracky-darkgray text-2xl">Email</h1>
             <input className="shadow-md w-full h-12 bg-emptracky-f5 rounded-[30px] px-5 border border-emptracky-blue" />
-            <h1 className="text-emptracky-darkgray text-[24px]">Phone Number</h1>
+            <h1 className="text-emptracky-darkgray text-2xl">Phone Number</h1>
             <input className="shadow-md w-full h-12 bg-emptracky-f5 rounded-[30px] px-5 border border-emptracky-blue" />
-            <h1 className="text-emptracky-darkgray text-[24px]">Password</h1>
+            <h1 className="text-emptracky-darkgray text-2xl">Password</h1>
             <input className="shadow-md w-full h-12 bg-emptracky-f5 rounded-[30px] px-5 border border-emptracky-blue" />
-            <h1 className="text-emptracky-darkgray text-[24px]">Confirm Password</h1>
+            <h1 className="text-emptracky-darkgray text-2xl">Confirm Password</h1>
             <input className="shadow-md w-full h-12 bg-emptracky-f5 rounded-[30px] px-5 border border-emptracky-blue" />
 
           </div>
@@ -45,7 +45,7 @@ const navigate = useNavigate();
               <h1 className="text-emptracky-f1 text-[26px]" onClick={() => navigate("/login")}>Sign up</h1>
             </button>
 
-            <h1 className="text-emptracky-darkgray text-[20px] ">Already have an account?
+            <h1 className="text-emptracky-darkgray text-lg text-center">Already have an account?
               <span className=" text-emptracky-blue text-decoration-line: underline hover:cursor-pointer w-fit transition-all duration-200 ml-2" onClick={() => navigate("/login")}>
                 Login here!
               </span>
